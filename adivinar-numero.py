ce7e3a756756f2b2cb1752f8607514762b8c6221
MaidanaEmi <emme.lq00@gmail.com>
2019-05-30 08:56:58 -0300

    Agrego clase validacion. Defino método que genera un aleatorio y método que calcula coincidencias entre dos listas

--- adivinar-numero.py
+++ adivinar-numero.py
@@ -5,21 +5,105 @@ import random
 #Clase Numero
 class Numero:
 
-    def __init__(self):
+    def __init__(self, numero):
         #self.numero = np.array(numero)
-        self.numero = []
+        self.numero = numero    
+    
+#Clase Juego
+class Juego:
+    #obj_numero = Numero()
+    #obj_random = Numero()
+
+    def __init__(self):
         self.cantCifras = 4
         self.error = ""
+        self.coinciden = False
     
-    def validarNumero(self,numeroi):
-        ve = self.validacionEntero(numeroi)
-        vc = self.validacionCantCifras(numeroi)
-        vr = self.validacionCifrasRepetidas(numeroi)
+    def iniciar_adivinar_numero(self):
+        correcto_ingresado = False
+        random_generado = self.generarRandom()
+        self.obj_random = Numero(random_generado)
+
+        while correcto_ingresado == False:
+            numero_ingresado = input('Ingresa un número de 4 cifras que no se repitan: ')
+            valido_ingresado = obj_validacion.validarNumero(numero_ingresado)
+            
+            if valido_ingresado == False:
+                print("Error:", obj_validacion.error)
+            else: 
+                ingresado_lista = [int(digit) for digit in numero_ingresado]
+                self.obj_ingresado = Numero(ingresado_lista)
+                resultado = self.calcularCoincidencias(self.obj_ingresado.numero,self.obj_random.numero)
+                if self.coinciden == True:
+                    print("¡Adivinaste!. Ese es el número")
+                    correcto_ingresado = True
+                    break
+                else:
+                    print("Cantidad de cifras correctas: ", resultado[0])
+                    print("Cantidad de cifras regulares: ", resultado[1])
+                    print("¡Intenta con otro número!")
+    
+    def Rand(self):
+        num = 4
+        start = 0
+        end = 9
+        random_lista = [] 
+        for j in range(num): 
+            random_lista.append(random.randint(start, end)) 
+        return random_lista
+
+    
+    def generarRandom(self):
+        valido = False        
+        while valido == False:
+            random_lista = self.Rand() #Creamos el aleatorio como una lista
+            validacion = obj_validacion.validacionCifrasRepetidas(random_lista)
+            if validacion == False:
+                valido = True            
+        return random_lista
+
+    def calcularCoincidencias(self,n1,n2):
+        correctas = 0
+        regulares = 0
+        if n1==n2:
+            self.coinciden = True
+        else:
+            self.coinciden = False
+            array1 = np.array(n1, dtype= 'int')
+            array2 = np.array(n2, dtype= 'int')
+            r = (array1==array2)
+            for i in r:
+                if i == True:
+                    correctas = correctas + 1
+            
+            for i in n1:           
+                if n2.count(i)==1:
+                    regulares = regulares+1
+                    i = 0
+                    while (i < 4):     
+                        if n1[i]==n2[i]:
+                            regulares = regulares -1
+                        i = i + 1
+
+        return (correctas,regulares)
+
+# Clase Validacion
+class Validacion:
+
+    def __init__(self):
+        self.cantCifras = 4
+        self.error = ""
+        
+    def validarNumero(self,numero_ingresado):
+       
+        ve = self.validacionEntero(numero_ingresado)
+        vc = self.validacionCantCifras(numero_ingresado)
+        vr = self.validacionCifrasRepetidas(numero_ingresado)
         if ve == False:
             self.error="¡Ingresa un número!"
             return False
         if vc == False:
-            self.error="¡Ingresa un número de {} cifras.!".format(self.cantCifras)
+            self.error="¡Ingresa un número de {} cifras! ".format(self.cantCifras)
             return False
         if  vr == True:
             self.error="¡Las cifras no deben repetirse!. Intenta con otro número."
@@ -27,7 +111,8 @@ class Numero:
         return True
 
     def validacionEntero(self,numero):
-        if numero.isdigit():
+        numero_str = str(numero) 
+        if numero_str.isdigit():
             return True
         else:
             return False
@@ -39,39 +124,20 @@ class Numero:
             return True
 
     def validacionCifrasRepetidas(self,numero):
-        arr = list(numero)
         contador = 0
-        for cifra in arr:
-            if arr.count(cifra)>1:
-                contador +=1
-                
+        for cifra in numero:
+            if numero.count(cifra)>1:
+                contador +=1                
         if contador > 1:
             return True
         else:
             return False
-#Clase Juego
-class Juego:
-    obj_numero = Numero()
-    #obj_random = Numero()
-
-    def __init__(self):
-        self.valido = False
-        self.countCorrectas = 0
-        self.countRegulares = 0
-        #self.nr = self.generarAleatorio()
-        #self.objrandom = Numero(self.nr)
-        #self.obj_numero = Numero(numeroi)
-    
-    def iniciar_juego(self):
-
-        while self.valido == False:
-            numeroi = input('Estoy pensando en un número. ¿Puedes adivinar cuál es?: ')
-            self.valido = self.obj_numero.validarNumero(numeroi)
-            if self.valido == False:
-                print("Error: {}".format(self.obj_numero.error))
-            else: 
-                print("Puedes jugar!")
-
 #Bloque principal
-obj_juego = Juego()
-obj_juego.iniciar_juego()
+obj_validacion = Validacion()
+print("Ingresa 1 si quieres ser el adivinador ")
+print("Ingresa 2 si quieres ser el pensador ")
+opcion = int(input("Opción: "))
+
+if opcion == 1:
+    obj_juego = Juego()
+    obj_juego.iniciar_adivinar_numero()
